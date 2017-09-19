db79109ef93485eba4282120ee94fdc70f30f2f5
Mike Chen <chenyunlong@msn.com>
2017-09-19 10:53:11 -0700

    f

--- samples/Demos/Ignite2017/create-a-policy-set/create.ps1
+++
@@ -1,3 +0,0 @@
-#install the latest powershell
-
-New-AzureRmPolicySetDefinition -Name BillingTags -PolicyDefinition C:\Users\mikech\Desktop\azurepolicyset.definitions.json -Parameter C:\Users\mikech\Desktop\azurepolicyset.parameters.json
