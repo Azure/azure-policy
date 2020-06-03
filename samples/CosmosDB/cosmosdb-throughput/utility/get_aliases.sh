a67d979790eb4d3ea16c88ae207a22892c496934
Patrick El-Azem <paelaz@microsoft.com>
2020-06-03 14:41:42 -0400

    Cosmos DB | Remove custom policy definition to govern throughput since superseded by built-in policy definition

--- samples/CosmosDB/cosmosdb-throughput/utility/get_aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Gets Azure Cosmos DB Resource Provider Aliases that can be used in a custom policy definition
-
-subscription_id="$(az account show -o tsv --query "id")"
-
-az provider show --namespace Microsoft.DocumentDB --expand "resourceTypes/aliases" --query "resourceTypes[].aliases[].name"
-
-az graph query -q "Resources | where type=~'microsoft.DocumentDB' | limit 1 | project aliases" --subscriptions $subscription_id
