814bc45b681ecf4abd6e5d6e2f0ee3cfa1979b41
Daniel Larsen <dalars@microsoft.com>
2020-12-04 09:48:59 +1300

    Fix comments and naming

--- samples/WebApp/web-app-vnet-route-all-enabled/deploy-policy.ps1
+++ samples/WebApp/web-app-vnet-route-all-enabled/deploy-policy.ps1
@@ -1,5 +1,5 @@
-$definition = New-AzPolicyDefinition -Name "web-app-vnet-route-all-enabled" -DisplayName "Enforce Web App VNet route all" `
-    -description "This policy ensures that all traffic originating from a web app is routed through Regional VNet integration" `
+$definition = New-AzPolicyDefinition -Name "audit-web-app-vnet-route-all-enabled" -DisplayName "Audit Web App VNet route all if not exists" `
+    -description "This policy audits that all traffic originating from a web app is routed through Regional VNet integration" `
     -Policy 'https://raw.githubusercontent.com/DanielLarsenNZ/azure-policy/master/samples/WebApp/web-app-vnet-route-all-enabled/azurepolicy.rules.json' `
     -Parameter 'https://raw.githubusercontent.com/DanielLarsenNZ/azure-policy/master/samples/WebApp/web-app-vnet-route-all-enabled/azurepolicy.parameters.json' `
     -Mode Indexed
@@ -7,5 +7,5 @@ $definition
 
 $scope = Get-AzResourceGroup -Name 'helloprivate-rg'
 $scope
-$assignment = New-AzPolicyAssignment -Name 'helloprivate-rg-web-app-vnet-route-all-enabled' -Scope $scope.ResourceId -PolicyDefinition $definition
+$assignment = New-AzPolicyAssignment -Name 'audit-helloprivate-rg-web-app-vnet-route-all-enabled' -Scope $scope.ResourceId -PolicyDefinition $definition
 $assignment
