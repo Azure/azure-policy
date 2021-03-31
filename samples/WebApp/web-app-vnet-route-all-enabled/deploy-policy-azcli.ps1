19ee8dbecaa4dc81cf3354bb371ab9b0b6af6b0b
Daniel Larsen <dalars@microsoft.com>
2021-03-31 21:16:19 +1300

    assignment name changes

--- samples/WebApp/web-app-vnet-route-all-enabled/deploy-policy-azcli.ps1
+++ samples/WebApp/web-app-vnet-route-all-enabled/deploy-policy-azcli.ps1
@@ -1,5 +1,5 @@
-$policy = 'audit-vnetrouteall-enabled-300320211631' 
-$assignment = 'hellovnetrouteall-rg-audit-vnetrouteall-enabled-300320211631'
+$policy = 'audit-vnetrouteall-enabled' 
+$assignment = 'hellovnetrouteall-rg-audit-vnetrouteall-enabled'
 
 az policy definition create --name $policy  `
     --display-name 'Audit Web App VNet route all if not exists' `
@@ -8,18 +8,9 @@ az policy definition create --name $policy  `
     --params 'https://raw.githubusercontent.com/DanielLarsenNZ/azure-policy/master/samples/WebApp/web-app-vnet-route-all-enabled/azurepolicy.parameters.json' `
     --mode Indexed
 
-az policy definition create --name "$policy-alias"  `
-    --display-name 'Audit Web App VNet route all if not exists' `
-    --description 'This policy audits that all traffic originating from a web app is routed through Regional VNet integration' `
-    --rules 'https://raw.githubusercontent.com/DanielLarsenNZ/azure-policy/master/samples/WebApp/web-app-vnet-route-all-enabled/azurepolicy-alias.rules.json' `
-    --params 'https://raw.githubusercontent.com/DanielLarsenNZ/azure-policy/master/samples/WebApp/web-app-vnet-route-all-enabled/azurepolicy.parameters.json' `
-    --mode Indexed
-
 $scope = ( az group show -n 'hellovnetrouteall-rg' | ConvertFrom-Json ).id
 $scope
 
 az policy assignment create --name $assignment --scope $scope --policy $policy
-
-az policy assignment create --name "$assignment-a" --scope $scope --policy "$policy-alias"
 
 #az policy state trigger-scan -g 'hellovnetrouteall-rg'
